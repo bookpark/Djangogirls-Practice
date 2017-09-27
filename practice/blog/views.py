cbab34938c40ff8eb9365dc7b0d0db7473369a80
Booki Park <bkbkgg@gmail.com>
2017-09-27 14:16:54 +0900

    Post List를 렌더링하는 View와 Templates 추가, url과 연결

--- practice/blog/views.py
+++ practice/blog/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    context = {
+        'posts': posts
+    }
+    return render(request, 'post_list.html', context)
--- practice/config/settings.py
+++ practice/config/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
+
     'blog',
 ]
 
@@ -56,7 +59,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
